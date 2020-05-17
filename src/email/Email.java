55fb850afc3e520b5fcbfeb0961d81c8784a6b87
jcc6611 <jcc6611@rit.edu>
2020-05-17 01:17:51 -0400

    Made classes

---
+++ src/email/Email.java
@@ -0,0 +1,15 @@
+package email;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String passWord;
+    private String department;
+    private int mailBoxCapacity;
+    private String altEmail;
+
+
+    public Email(String firstName, String lastName){
+
+    }
+}
